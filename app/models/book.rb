3dac5f76a0a71c931f76c89b1d0976cf8fe5e5e2
ShutoIto <175019209+shuto822@users.noreply.github.com>
2024-10-03 16:02:50 +0000

    応用課題５

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,4 +10,16 @@ class Book < ApplicationRecord
   def favorite_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+  
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('name LIKE ?', '%' + content)
+    else
+      Book.where('name LIKE ?', '%' + content + '%')
+    end 
+  end 
 end
